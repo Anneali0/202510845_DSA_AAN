f547e72fcbd098e7ea7d8af138ff7bf5224d4115
Anne <anan123ali123@gmail.com>
2026-05-14 18:05:40 +0200

    Add files via upload

---
+++ congradulate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+    void congradulate(string Name){
+cout<<"congradulations "<<Name<<"!";
+};
+int main(){
+string userName;
+cout<<"Enter your name:";
+cin>>userName;
+congradulate(userName);
+
+    return 0;
+}
